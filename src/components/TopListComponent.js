f356617e574bbd1d7b34aeb0040a0d809b091694
Sabina Timpea <sabina.tmp@gmail.com>
2021-01-27 19:04:53 +0000

    cleanup top list component

--- src/components/TopListComponent.js
+++ src/components/TopListComponent.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
@@ -15,20 +12,10 @@ const useStyles = makeStyles((theme) => ({
       textDecoration: 'none',
       color: theme.palette.primary.dark
     },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
     card: {
       height: '100%',
       display: 'flex',
       flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
     },
   }));
   
@@ -45,20 +32,19 @@ export default function TopList() {
     }, [])
 
     return (
-        <Grid item key={1} xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-            <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                Top places to visit
-                </Typography>
-                {list.map((item, index) => (
-                  <Link key={index} className={classes.link} to={`/results/${item.toLowerCase()}`}>
-                    <Typography>{item}</Typography>
-                  </Link>
-                ))}
-            </CardContent>
-            </Card>
-        </Grid>
-          
+      <Grid item key={1} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+          <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+              Top places to visit
+              </Typography>
+              {list.map((item, index) => (
+                <Link key={index} className={classes.link} to={`/results/${item.toLowerCase()}`}>
+                  <Typography>{item}</Typography>
+                </Link>
+              ))}
+          </CardContent>
+          </Card>
+      </Grid>
     );
 };
