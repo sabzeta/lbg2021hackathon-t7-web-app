00ed81bdfd179202592dfe71178c41934f8ed95c
Sabina Timpea <sabina.tmp@gmail.com>
2021-01-27 19:05:10 +0000

    cleanup map component

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
@@ -29,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Map() {
     const classes = useStyles();
 
+
     return (
         <Grid item key={1} xs={12} sm={6} md={8}>
             <Card className={classes.card}>
@@ -37,7 +35,7 @@ export default function Map() {
                 Map
                 </Typography>
                 <Typography>
-                This will be a gmap integration
+                This will be a map integration
                 </Typography>
             </CardContent>
             </Card>
