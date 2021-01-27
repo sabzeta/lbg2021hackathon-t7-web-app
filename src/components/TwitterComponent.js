22cd28d827d5e3759bfced54b5150fda50dd392a
Sabina Timpea <sabina.tmp@gmail.com>
2021-01-27 19:05:44 +0000

    add twitter and a todo list

---
+++ src/components/TwitterComponent.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+      textDecoration: 'none',
+      color: theme.palette.primary.dark
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+  }));
+  
+export default function Twitter({ place }) {
+    const classes = useStyles();
+    const [value, setValue] = useState([]);
+
+    useEffect(() => {
+      const getList = async () => {
+        const response = await axios.get(`https://europe-west2-lbghack2021team7.cloudfunctions.net/web-api-sentiment?place=${place}`);
+        setValue(response.data);
+      };
+      getList();
+    }, [])
+
+    return (
+      <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+          Twitter sentiment
+          </Typography>
+          <Typography>{value}</Typography>
+      </CardContent>
+    );
+};
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -3,8 +3,29 @@ import { makeStyles } from '@material-ui/core/styles';
 import {
   useRouteMatch,
 } from "react-router-dom";
+import Twitter from '../components/TwitterComponent';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.primary.dark
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
 }));
   
 export default function ResultsPage() {
@@ -12,6 +33,24 @@ export default function ResultsPage() {
     let { params } = useRouteMatch("/results/:place");
 
     return (
-        `Results for ${params.place}`
+      <>
+        
+        <Grid item key={1} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <Twitter place={params.place} />
+          </Card>
+        </Grid>
+
+        <Grid item key={1} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Where to eat?
+              </Typography>
+              <Typography>Comming up next!</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </>
     );
 };
